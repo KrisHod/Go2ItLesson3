27bfa0e4192a9ae5ddb997e5c81bf267b5afe4ce
Kris <krisgodom@gmail.com>
2020-11-20 19:01:17 -0400

    added version with ArrayList in Task3_1_3

--- src/com/go2It/Lesson3/Task3_1_2.java
+++ src/com/go2It/Lesson3/Task3_1_2.java
@@ -1,14 +1,50 @@
 package com.go2It.Lesson3;
 
+import java.util.ArrayList;
+
 public class Task3_1_2 {
     public static void main(String[] args) {
-        int [] num = new int [100];
-        int [] divisibleBy6 = new int[50];
-        for (int i = 0,  j = 1; i<100 && j<100; i++, j+=3){
-            if (j % 6!=0) {
-                num[i] = j;
-                System.out.println(num[i]);
+        // version without ArrayList
+//        int [] num = new int [100];
+//        int [] divisibleBy6 = new int[50];
+//        for (int i = 0,  j = 0; i<100 && j<100; i++, j+=3){ // loop for initializing array with arithmetic progression +3
+//            if (j % 6!=0) {                                 // without digitals divided by 6
+//                num[i] = j;
+//                i--;
+//            }
+//            else {
+//                divisibleBy6[i]=j;
+//            }
+//        }
+//
+//        for (int arr:num) {
+//            System.out.println(arr);
+//        }
+//
+//        for (int arr:divisibleBy6) {
+//            System.out.println(arr);
+//        }
+//        System.out.println(divisibleBy6.length);
+
+        // version with ArrayList
+        ArrayList<Integer> num = new ArrayList<>();
+        ArrayList<Integer> divisibleBy6 = new ArrayList<>();
+        for (int i = 0, j = 0; i < 100 && j < 100; i++, j += 3) {
+            if (j % 6 != 0) {                                 // without digitals divided by 6
+                num.add(j);
+                i--;
+            } else {
+                divisibleBy6.add(j);
             }
         }
+
+        for (int arr : num) {
+            System.out.println(arr);
+        }
+
+        for (int arr : divisibleBy6) {
+            System.out.println(arr);
+        }
+        System.out.println(divisibleBy6.size());
     }
 }
