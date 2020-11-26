b2eca139bef750fc3dbab34d152b61b3ed434155
Kris <krisgodom@gmail.com>
2020-11-26 14:22:59 -0400

    debugging2

--- src/com/go2It/Lesson3/Task3_2_4.java
+++ src/com/go2It/Lesson3/Task3_2_4.java
@@ -6,29 +6,28 @@ import java.util.List;
 public class Task3_2_4 {
 
     public static void showLeftDays(DAY_OF_WEEK currentDay) {
-        if (currentDay.ordinal()==5 || currentDay.ordinal()==6) {
+        if (currentDay.ordinal() == 5 || currentDay.ordinal() == 6) {
             System.out.println("Horray! It's weekend!");
         } else {
             System.out.println("There are " + (4 - currentDay.ordinal()) + " days till the weekend");
         }
     }
 
-    public static List<DAY_OF_WEEK> returnArray (DAY_OF_WEEK currentDay){
+    public static List<DAY_OF_WEEK> returnArray(DAY_OF_WEEK currentDay) {
         DAY_OF_WEEK[] arrOfAllDays = DAY_OF_WEEK.values();
         List<DAY_OF_WEEK> daysOfWeekList = Arrays.asList(arrOfAllDays);
-        List<DAY_OF_WEEK> daysLeftList = daysOfWeekList.subList(currentDay.ordinal()+1, DAY_OF_WEEK.SATURDAY.ordinal());
+        List<DAY_OF_WEEK> daysLeftList = daysOfWeekList.subList(currentDay.ordinal() + 1, DAY_OF_WEEK.SATURDAY.ordinal());
         return daysLeftList;
     }
 
     // overloaded method showLeftDays
-    public static void showLeftDays(DAY_OF_WEEK currentDay, int additionalDaysOf){
-        if (currentDay.ordinal()==5 || currentDay.ordinal()==6) {
+    public static void showLeftDays(DAY_OF_WEEK currentDay, int additionalDaysOf) {
+        if (currentDay.ordinal() == 5 || currentDay.ordinal() == 6) {
             System.out.println("Horray! It's weekend!");
         } else {
             System.out.println("There are " + (4 - additionalDaysOf - currentDay.ordinal()) + " days till the weekend");
         }
     }
-
 
 
     public static void main(String[] args) {
--- src/com/go2It/Lesson3/TemperatureConvertor.java
+++ src/com/go2It/Lesson3/TemperatureConvertor.java
@@ -6,21 +6,21 @@ public class TemperatureConvertor {
     public static String convertTemperatureFromCToF(double temp) {
         double res = temp * 1.8 + 32;
         double roundedRes = Math.round(res * 10) / 10.0;
-        String result = temp + "F equals to " + roundedRes + "C.";
+        String result = temp + "C equals to " + roundedRes + "F.";
         return result;
     }
 
     public static String convertTemperatureFromFToC(double temp) {
         double res = (temp - 32) / 1.8;
         double roundedRes = Math.round(res * 10) / 10.0;
-        String result = temp + "C equals to " + roundedRes + "F.";
+        String result = temp + "F equals to " + roundedRes + "C.";
         return result;
     }
 
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter the target temperature");
+        System.out.println("Please enter the temperature");
         int temp = in.nextInt();
         System.out.println("Please enter the target scale (F or C)");
         String scale = in.next();
@@ -29,7 +29,18 @@ public class TemperatureConvertor {
         } else if (scale.equalsIgnoreCase("C")) {
             System.out.println(convertTemperatureFromFToC(temp));
         } else {
-            System.out.println("Invalid input");
+            int i = 0;
+            while (!(scale.equalsIgnoreCase("F") || scale.equalsIgnoreCase("C"))) {
+                System.out.println("Please enter the target scale (F or C) one more time");
+                String rightScale = in.next();
+                if (rightScale.equalsIgnoreCase("F")) {
+                    System.out.println(convertTemperatureFromCToF(temp));
+                    break;
+                } else if (rightScale.equalsIgnoreCase("C")) {
+                    System.out.println(convertTemperatureFromFToC(temp));
+                    break;
+                }
+            }
         }
     }
 }
